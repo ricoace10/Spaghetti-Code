a9110329886a5711303fc9df29a9b805cae0c04e
ricoace10 <94618629+ricoace10@users.noreply.github.com>
2024-04-10 19:22:56 +0200

    Inital comit

---
+++ clean.js
@@ -0,0 +1,15 @@
+var budget = [
+  { value: 250, description: "Sold old TV 📺", user: "david" },
+  { value: -45, description: "Groceries 🥑", user: "david" },
+  { value: 3500, description: "Monthly salary 👩‍💻", user: "david" },
+  { value: 300, description: "Freelancing 👩‍💻", user: "david" },
+  { value: -1100, description: "New iPhone 📱", user: "david" },
+  { value: -20, description: "Candy 🍭", user: "matilda" },
+  { value: -125, description: "Toys 🚂", user: "matilda" },
+  { value: -1800, description: "New Laptop 💻", user: "david" },
+];
+
+var limits = {
+  david: 1500,
+  matilda: 100,
+};
