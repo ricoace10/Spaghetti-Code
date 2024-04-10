f4e6cdb76bccb3e381c7d709b1ea225c6654e104
ricoace10 <94618629+ricoace10@users.noreply.github.com>
2024-04-10 21:54:30 +0200

    Js Configuration

--- clean.js
+++ clean.js
@@ -13,3 +13,52 @@ var limits = {
   david: 1500,
   matilda: 100,
 };
+
+var add = function (value, description, user) {
+  if (!user) user = "david";
+  user = user.toLowerCase();
+
+  var lim;
+  if (limits[user]) {
+    lim = limits[user];
+  } else {
+    lim = 0;
+  }
+
+  if (value <= lim) {
+    budget.push({ value: -value, description: description, user: user });
+  }
+};
+add(10, "Pizza 🍕");
+add(100, "Going to movies 🍿", "Matilda");
+add(200, "Stuff", "Jay");
+console.log(budget);
+
+var check = function () {
+  for (var el of budget) {
+    var lim;
+    if (limits[el.user]) {
+      lim = limits[el.user];
+    } else {
+      lim = 0;
+    }
+
+    if (el.value < -lim) {
+      el.flag = "limit";
+    }
+  }
+};
+check();
+
+console.log(budget);
+
+var bigExpenses = function (limit) {
+  var output = "";
+  for (var el of budget) {
+    if (el.value <= -limit) {
+      output += el.description.slice(-2) + " / "; // Emojis are 2 chars
+    }
+  }
+  output = output.slice(0, -2); // Remove last '/ '
+  console.log(output);
+};
