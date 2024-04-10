cc3ccd7aee50526d19b2c8d06d3284663bf59375
ricoace10 <94618629+ricoace10@users.noreply.github.com>
2024-04-11 00:21:46 +0200

    Analysis

--- clean.js
+++ clean.js
@@ -1,4 +1,5 @@
 // Using Var instead of const as a variable
+// const would have been a better option istead of var in this case.
 var budget = [
   { value: 250, description: "Sold old TV 📺", user: "david" },
   { value: -45, description: "Groceries 🥑", user: "david" },
@@ -10,7 +11,8 @@ var budget = [
   { value: -1800, description: "New Laptop 💻", user: "david" },
 ];
 
-// Use meaningful and pronounceable variable names
+// Use explanatory variables (Bad Example Here)
+// I could change the variable name to a more descriptive one so the code is easier to read.
 var limits = {
   david: 1500,
   matilda: 100,
@@ -21,6 +23,7 @@ var add = function (value, description, user) {
   user = user.toLowerCase();
 
   // Using deafault parameters instead of short circuiting or conditionals.
+  // A ternary operator can be use here instead of these if and else statements.
   var lim;
   if (limits[user]) {
     lim = limits[user];
@@ -28,26 +31,33 @@ var add = function (value, description, user) {
     lim = 0;
   }
 
+  // Unnecesarry repetition here
+  // The code will still work thesame without repetition with help of the enhance object literal syntax because the property name is thesame as the variable name.
   if (value <= lim) {
     budget.push({ value: -value, description: description, user: user });
   }
 };
+
 add(10, "Pizza 🍕");
 add(100, "Going to movies 🍿", "Matilda");
 add(200, "Stuff", "Jay");
 console.log(budget);
 
+// Function Arguments
+
+// Could have used a better name here for the function.
+// A ternary operator can be use here instead of these if and else statements.
 var check = function () {
-  for (var el of budget) {
+  for (var entry of budget) {
     var lim;
-    if (limits[el.user]) {
-      lim = limits[el.user];
+    if (limits[entry.user]) {
+      lim = limits[entry.user];
     } else {
       lim = 0;
     }
 
-    if (el.value < -lim) {
-      el.flag = "limit";
+    if (entry.value < -lim) {
+      entry.flag = "limit";
     }
   }
 };
@@ -57,8 +67,8 @@ console.log(budget);
 
 var bigExpenses = function (limit) {
   var output = "";
-  for (var el of budget) {
-    if (el.value <= -limit) {
+  for (var entry of budget) {
+    if (entry.value <= -limit) {
       output += el.description.slice(-2) + " / "; // Emojis are 2 chars
     }
   }
